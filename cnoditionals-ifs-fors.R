90d2319f930243924a7cc3ff76e263ff0020e22e
SRKads1998 <kadonoffs@gmail.com>
2025-08-13 12:32:51 -0700

    for loo

--- cnoditionals-ifs-fors.R
+++ cnoditionals-ifs-fors.R
@@ -57,3 +57,67 @@ switch (species,
 )
 
 # see next week dplyr:: case_when!
+
+# writing for loops
+
+dog_names <- c("Teddy", "Khora", "Banjo","Waffle")
+# basic version below
+print(paste("My pup's name shall be", dog_names[1]))
+
+pupster <- dog_names[1]
+print(paste("My pup's name shall be", dog_names[pupster]))
+
+for (pupster in dog_names)  {
+  print(paste("My dog's name is", pupster))
+
+}
+
+# another one
+
+mass <- seq(from = 0, to = 3, by = 0.5)
+
+i <- mass[1]
+i
+new_val <- i + 2 #adding 2 to each value
+print(new_val) #printing the new value
+
+# write into a for loop
+for (i in mass) {
+  new_val <- i + 2
+  print(new_val)
+  
+}
+
+# practicing indexing
+
+new_val <- mass[i] + 2
+print(new_val)
+
+# write into a for loop
+
+for (i in 1:length(mass)) {
+  new_val <- mass[i] + 2
+  print(new_val)
+}
+
+seq(new_val)
+seq_along(mass)
+length(mass)
+
+# anotehr example with  iterating by position (indexing)
+tree_height <- c(1, 2, 6, 10)
+
+# exxample for the frist case
+i <- 1
+
+
+# convert into a generalizable expression
+tree_height [i] + tree_height[i + 1]
+
+# conver into a for loop and test out making a sequence
+seq_along(tree_height)
+
+for (i in seq_along(tree_height)) {
+  val <- tree_height [i] + tree_height [i +1]
+  print(val)
+}
